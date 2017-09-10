4e07e79ea628c9b6ab388b9847c77b8044b25fff
Brian Rotich <brian.rotich@andela.com>
2017-09-10 22:55:40 +0300

    (chore) add kafka producter when endpoint is reached

--- run.py
+++ run.py
@@ -1,13 +1,42 @@
 from flask import Flask
 from redis import Redis
+from kafka import SimpleProducer, KafkaClient, KafkaConsumer
+
+import json
+
+from threading import Thread
 
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
+
+kafka = KafkaClient('kafka:9092')
+
+topic = 'one_tets'
+
+# consumer = KafkaConsumer(topic, group_id='download', bootstrap_servers=['kafka:9092'],
+#                         auto_offset_reset = 'earliest',
+#                         value_deserializer = lambda m: json.loads(m.decode()))
+producer = SimpleProducer(kafka)
+
 
 @app.route('/')
 def hello():
     count = redis.incr('hits')
     return 'Hello World! I have been seen {} times.\n'.format(count)
 
+@app.route('/send')
+def sendTopic():
+    
+    data = {
+        'name': 'test',
+        'message': 'recieved'
+    }
+    message = json.dumps(data, indent=2)
+    producer.send_messages(topic, message.encode())
+
+    return 'send message kafka'
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
+
+    
